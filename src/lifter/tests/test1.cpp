3f35a3b44fea5a5754d18e410792b6a53ba1ecee
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-05-13 18:40:05 +0200

    Add tests using the googletest framework and integrate with gitlab-ci
    This commit is expected to fail, to test the Tests output integration
    with gitlab.
    The coverage / test artifacts are also included in the
    downloadable/browsable artifacts.

---
+++ src/lifter/tests/test1.cpp
@@ -0,0 +1,20 @@
+#include "lifter/lifter.h"
+
+#include "gtest/gtest.h"
+
+namespace lifter {
+
+class Test1 : public ::testing::Test {
+
+};
+
+TEST_F(Test1, test1) {
+    EXPECT_EQ(foo(), true) << "foo() not implemented correctly";
+}
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
