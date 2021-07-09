33df8453929251aecea3772ee13c8d45aae95c9f
Johannes Maier <johannes2.maier@tum.de>
2021-07-09 12:52:38 +0200

    Working on big test

--- src/lifter/tests/test_split_basic_block.cpp
+++ src/lifter/tests/test_split_basic_block.cpp
@@ -53,17 +53,17 @@ TEST(SPLIT_BASIC_BLOCK_TEST, test_small) {
 
     // lifting some instructions to generate some variables
     // instructions = (addi x2, x0, 50), (andi x3, x2, 16), (sub x2, x3, x2)
-    RV64Inst instrcutions[3] = {RV64Inst{FrvInst{FRV_ADDI, 2, 0, 0, 0, 0, 50}, 0}, RV64Inst{FrvInst{FRV_ANDI, 3, 2, 0, 0, 0, 16}, 0}, RV64Inst{FrvInst{FRV_SUB, 2, 3, 2, 0, 0, 0}, 0}};
+    RV64Inst instructions[3] = {RV64Inst{FrvInst{FRV_ADDI, 2, 0, 0, 0, 0, 50}, 0}, RV64Inst{FrvInst{FRV_ANDI, 3, 2, 0, 0, 0, 16}, 0}, RV64Inst{FrvInst{FRV_SUB, 2, 3, 2, 0, 0, 0}, 0}};
 
     std::vector<int> count_var_per_instr{};
     {
-        int currr_addr = bb_start_addr;
+        int curr_ip = bb_start_addr;
         size_t previous_size = count_static_vars;
-        for (RV64Inst &instr : instrcutions) {
-            lifter.parse_instruction(instr, block, mapping, currr_addr, currr_addr + 4);
+        for (RV64Inst &instr : instructions) {
+            lifter.parse_instruction(instr, block, mapping, curr_ip, curr_ip + 4);
             count_var_per_instr.push_back((int)(block->variables.size() - previous_size));
             previous_size = block->variables.size();
-            currr_addr += 4;
+            curr_ip += 4;
         }
     }
     // split the block
@@ -112,5 +112,84 @@ TEST(SPLIT_BASIC_BLOCK_TEST, test_small) {
 }
 
 TEST(SPLIT_BASIC_BLOCK_TEST, test_big) {
+    // define constants used for test
+    const size_t prog_start = 0;
+    const size_t prog_end = 100;
 
+    const size_t bb_start_addr = 8;
+    // the instruction is in the first basic block, the remainder is in the second one
+    const size_t bb_split_addr = 24;
+    const size_t bb_end_addr = 36;
+
+    IR ir{};
+    ir.setup_bb_addr_vec(prog_start, prog_end);
+
+    // add static mapper
+    for (int i = 0; i < 33; i++) {
+        ir.add_static(Type::i64);
+    }
+
+    Lifter lifter{&ir};
+
+    // create a basic block which should be splitted
+    BasicBlock *block = ir.add_basic_block(bb_start_addr, "test_block_1");
+
+    block->set_virt_end_addr(bb_end_addr);
+
+    reg_map mapping{};
+
+    // add statics
+    for (unsigned long i = 1; i < mapping.size(); i++) {
+        mapping.at(i) = block->add_var_from_static(i, bb_start_addr);
+    }
+
+    const size_t count_static_vars = block->variables.size();
+
+    // create instructions
+    RV64Inst instructions[11];
+
+    // addi x2, x0, 69
+    instructions[0] = RV64Inst{FrvInst{FRV_ADDI, 2, 0, 0, 0, 0, 69}, 0};
+    // addi x3, x0, 42
+    instructions[1] = RV64Inst{FrvInst{FRV_ADDI, 3, 0, 0, 0, 0, 42}, 0};
+    // addi x4, x0, 1337
+    instructions[2] = RV64Inst{FrvInst{FRV_ADDI, 4, 0, 0, 0, 0, 1337}, 0};
+    // addi x5, x0, 420
+    instructions[3] = RV64Inst{FrvInst{FRV_ADDI, 5, 0, 0, 0, 0, 420}, 0};
+
+    // xor x6, x2, x3
+    instructions[4] = RV64Inst{FrvInst{FRV_XOR, 6, 2, 3, 0, 0, 0}, 0};
+    // xor x7, x4, x5
+    instructions[5] = RV64Inst{FrvInst{FRV_XOR, 7, 4, 5, 0, 0, 0}, 0};
+
+    // add x3, x3, x2
+    instructions[6] = RV64Inst{FrvInst{FRV_ADD, 3, 3, 2, 0, 0, 0}, 0};
+    // add x5, x5, x4
+    instructions[7] = RV64Inst{FrvInst{FRV_ADD, 5, 5, 4, 0, 0, 0}, 0};
+
+    // and x6, x6, x3
+    instructions[8] = RV64Inst{FrvInst{FRV_AND, 6, 6, 3, 0, 0, 0}, 0};
+    // and x7, x7, x5
+    instructions[9] = RV64Inst{FrvInst{FRV_AND, 7, 7, 5, 0, 0, 0}, 0};
+
+    // sub x7, x7, x6
+    instructions[10] = RV64Inst{FrvInst{FRV_SUB, 7, 7, 6, 0, 0, 0}, 0};
+
+    uint64_t count_vars_before = 0;
+    uint64_t count_vars_after = 0;
+    {
+        uint64_t curr_ip = bb_start_addr;
+        for (RV64Inst &instr : instructions) {
+            lifter.parse_instruction(instr, block, mapping, curr_ip, curr_ip + 4);
+            if (curr_ip >= bb_split_addr) {
+                count_vars_after = block->variables.size() - count_static_vars - count_vars_before;
+            } else {
+                count_vars_before = block->variables.size() - count_static_vars;
+            }
+            curr_ip += 4;
+        }
+    }
+
+    std::cout << count_vars_before << "\n";
+    std::cout << count_vars_after << "\n";
 }
