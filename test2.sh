7d5d75817bc3856054aed3582ae04a6137437b4f
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-07-15 16:49:24 +0200

    test2.sh: update script

--- test2.sh
+++ test2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# parts taken from https://github.com/riscv/riscv-gnu-toolchain/blob/master/Makefile.in
+# You may need any of the following dependencies (taken from myunix3):
+# sudo apt-get -y install make curl gpg xz-utils patch gcc g++ libmpfr-dev libmpc-dev grub-common grub-pc-bin xorriso zlib1g zlib1g-dev mtools
 
 set -e
 set -x
 
 mkdir big_tests
 pushd big_tests
+
+# Download dependencies
 
 wget "https://ftp.gnu.org/gnu/binutils/binutils-2.35.2.tar.xz"
 wget "https://ftp.gnu.org/gnu/binutils/binutils-2.35.2.tar.xz.sig"
@@ -17,7 +20,11 @@ wget "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz.sig"
 wget "https://musl.libc.org/releases/musl-1.2.2.tar.gz"
 wget "https://musl.libc.org/releases/musl-1.2.2.tar.gz.asc"
 
+# Verify checksums
+
 sha256sum --check ../downloads.sha256sum
+
+# Extract dependencies
 
 tar -xvf binutils-2.35.2.tar.xz
 
@@ -29,13 +36,19 @@ SYSROOT="$PWD/sysroot" # XXX: symlink to build musl libc before running this scr
 PATH="${SYSROOT}/bin:$PATH"
 export PATH
 
+CONF_HOST="x86_64-linux-gnu"
+CONF_BUILD="${CONF_HOST}"
+CONF_TARGET="riscv64-linux-gnu"
+
+# Build binutils
+
 mkdir binutils-build
 pushd binutils-build
 
 ../binutils-2.35.2/configure \
-    --target="riscv64-linux-gnu" \
-    --host="x86_64-linux-gnu" \
-    --build="x86_64-linux-gnu" \
+    --host="${CONF_HOST}" \
+    --build="${CONF_BUILD}" \
+    --target="${CONF_TARGET}" \
     --prefix="${SYSROOT}" \
     --disable-gdb \
     --disable-readline \
@@ -44,6 +57,8 @@ popd
 
 make -C binutils-build -j3 --output-sync all
 make -C binutils-build -j3 --output-sync install
+
+# Build gcc step 1
 
 # Building in the source directory is buggy / does not work
 mkdir gcc-build
@@ -57,10 +72,18 @@ mkdir -p "${SYSROOT}"
 # checked by make all-gcc
 mkdir -p "${SYSROOT}/include"
 
-# Mostly taken from myunix2 mk/toolchain.mk:
+# Mostly taken from myunix3 mk/toolchain.mk or gcc --verbose on debian
 # --enable-multilib: build different libgcc to support -march (i think)
-# --with-sysroot: gcc needs headers from the target ._:
+# --with-sysroot: gcc needs headers from the target:
+#
+# parts taken from https://github.com/riscv/riscv-gnu-toolchain/blob/master/Makefile.in
+# other parts from myunix3
+#
+# We also specifc --with-abi / --with-arch to select the abi libgcc should use
 ../gcc-10.2.0/configure \
+    --host="${CONF_HOST}" \
+    --build="${CONF_BUILD}" \
+    --target="${CONF_TARGET}" \
     --enable-languages=c \
     --disable-shared \
     --disable-libitm \
@@ -73,9 +96,6 @@ mkdir -p "${SYSROOT}/include"
     --disable-multilib \
     --with-arch=rv64imafdc \
     --with-abi=lp64 \
-    --build="x86_64-linux-gnu" \
-    --host="x86_64-linux-gnu" \
-    --target="riscv64-linux-gnu" \
     --prefix="${SYSROOT}" \
     --disable-nls \
     --with-system-zlib \
@@ -86,29 +106,27 @@ mkdir -p "${SYSROOT}/include"
 popd
 
 # build riscv64-linux-gnu-gcc cross compiler
+# Seperate make invocations because gccs Makefile is fragile
 make -C gcc-build -j3 --output-sync all-gcc
 make -C gcc-build --output-sync install-gcc
 
-# configure musl libc
+# Build musl-libc
+
 pushd musl-1.2.2
-CROSS_COMPILE="riscv64-linux-gnu-" \
-    CFLAGS="-march=rv64iac" \
-    ./configure \
-        --target=riscv64-linux-gnu \
-        --disable-shared \
-        --prefix="${SYSROOT}"
+CFLAGS="-march=rv64iac" \
+./configure \
+    --target=riscv64-linux-gnu \
+    --disable-shared \
+    --prefix="${SYSROOT}"
 popd
 
-# make -C musl-1.2.2 -j3 --output-sync install-headers
 make -C musl-1.2.2 -j3 --output-sync all
 make -C musl-1.2.2 -j3 --output-sync install
+
+# Build gcc step 2
 
 # build libgcc using installed headers
 make -C gcc-build -j3 --output-sync all-target-libgcc
 make -C gcc-build -j3 --output-sync install-target-libgcc
-
-# finish musl libc
-#make -C musl-1.2.2 -j3 --output-sync all
-#make -C musl-1.2.2 -j3 --output-sync install
 
 popd
