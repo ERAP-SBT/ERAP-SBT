59f484803882d84aaca4cd0aee9f19170394ebaf
Johannes Maier <johannes2.maier@tum.de>
2021-08-22 20:36:09 +0200

    Partly implemented lifting for floating point classify instruction

--- include/lifter/lifter.h
+++ include/lifter/lifter.h
@@ -122,6 +122,8 @@ class Lifter {
 
     static void lift_float_comparison(BasicBlock *bb, const RV64Inst &instr, reg_map &mapping, uint64_t ip, const Instruction instruction_type, const Type op_size);
 
+    static void lift_fclass(BasicBlock *bb, const RV64Inst &instr, reg_map &mapping, uint64_t ip, const Type op_size);
+
     // helpers for lifting and code reduction
     static SSAVar *load_immediate(BasicBlock *bb, int64_t imm, uint64_t ip, bool binary_relative);
 
