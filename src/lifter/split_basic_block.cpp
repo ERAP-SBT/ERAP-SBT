d3d463259b6a65b6ffe564706b142f5ab70ec31b
Felix Solcher <felix.solcher@tum.de>
2021-08-24 12:37:02 +0200

    Encapsulate SSAVar::info

--- src/lifter/split_basic_block.cpp
+++ src/lifter/split_basic_block.cpp
@@ -212,8 +212,7 @@ BasicBlock *Lifter::split_basic_block(BasicBlock *bb, uint64_t addr, ELF64File *
         SSAVar *var = (*it).get();
 
         // variable is the result of an Operation -> adjust the inputs of the operation
-        if (std::holds_alternative<std::unique_ptr<Operation>>(var->info)) {
-            auto *operation = std::get<std::unique_ptr<Operation>>(var->info).get();
+        if (auto *operation = var->maybe_get_operation()) {
             for (auto &in_var : operation->in_vars) {
                 // skip nullptrs
                 if (in_var == nullptr) {
