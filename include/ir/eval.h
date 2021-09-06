d253c95a9ed036b367fdfa8e4199254bdb73a365
Felix Solcher <felix.solcher@tum.de>
2021-09-06 21:38:07 +0200

    const-folding: add (u)min,max and slt(u)

--- include/ir/eval.h
+++ include/ir/eval.h
@@ -11,4 +11,6 @@ uint64_t eval_morphing_op(Instruction insn, Type from, Type to, uint64_t raw);
 std::pair<uint64_t, uint64_t> eval_div(Instruction insn, Type type, uint64_t a, uint64_t b);
 
 bool typed_equal(Type type, uint64_t a, uint64_t b);
+/** Returns -1, 0 or 1 if a is less than, equal to, or greater than b */
+int typed_compare(Type type, uint64_t a, uint64_t b, bool signed_compare);
 uint64_t typed_narrow(Type type, uint64_t value);
