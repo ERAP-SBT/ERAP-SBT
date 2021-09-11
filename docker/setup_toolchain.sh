a36f0faaf481245494e7c382761a5d8701c755f3
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-09-11 11:29:45 +0200

    docker: make PREFIX a relative path and other improvements
    Using an absolute path for PREFIX has the potential of breaking some
    things (e.g. building glibc).

--- docker/setup_toolchain.sh
+++ docker/setup_toolchain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # You need the following dependencies:
-# sudo apt-get install -y libmpfr-dev libmpc-dev zlib1g zlib1g-dev
+# sudo apt-get install -y libmpfr-dev libmpc-dev zlib1g-dev
 
 set -e
 set -x
@@ -13,14 +13,9 @@ pushd toolchain
 
 # Download dependencies
 
-wget "https://ftp.gnu.org/gnu/binutils/binutils-2.35.2.tar.xz"
-wget "https://ftp.gnu.org/gnu/binutils/binutils-2.35.2.tar.xz.sig"
-
-wget "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
-wget "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz.sig"
-
-wget "https://musl.libc.org/releases/musl-1.2.2.tar.gz"
-wget "https://musl.libc.org/releases/musl-1.2.2.tar.gz.asc"
+wget --quiet "https://ftp.gnu.org/gnu/binutils/binutils-2.35.2.tar.xz"
+wget --quiet "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
+wget --quiet "https://musl.libc.org/releases/musl-1.2.2.tar.gz"
 
 # Verify checksums
 
@@ -29,13 +24,13 @@ sha256sum --check ../downloads.sha256sum
 # Extract dependencies
 
 tar -xvf binutils-2.35.2.tar.xz
-
 tar -xvf gcc-10.2.0.tar.xz
-
 tar -xvf musl-1.2.2.tar.gz
 
-SYSROOT="$PWD/sysroot" # XXX: symlink to build musl libc before running this script
-PATH="${SYSROOT}/bin:$PATH"
+SYSROOT="/toolchain"
+PREFIX="/usr"
+JOBS="-j$(nproc) --output-sync"
+PATH="${SYSROOT}${PREFIX}/bin:$PATH"
 export PATH
 
 CONF_HOST="x86_64-linux-gnu"
@@ -51,14 +46,14 @@ pushd binutils-build
     --host="${CONF_HOST}" \
     --build="${CONF_BUILD}" \
     --target="${CONF_TARGET}" \
-    --prefix="${SYSROOT}" \
+    --prefix="${SYSROOT}${PREFIX}" \
     --disable-gdb \
     --disable-readline \
     --with-system-zlib
 popd
 
-make -C binutils-build -j3 --output-sync all
-make -C binutils-build -j3 --output-sync install
+make -C binutils-build $(JOBS) all
+make -C binutils-build $(JOBS) install
 
 # Build gcc step 1
 
@@ -69,10 +64,10 @@ pushd gcc-build
 # You can use this to also build mpcr, etc.. if you don't have them installed
 #./contrib/download_prerequisites
 
-mkdir -p "${SYSROOT}"
+mkdir -p "${SYSROOT}${PREFIX}"
 
 # checked by make all-gcc
-mkdir -p "${SYSROOT}/include"
+mkdir -p "${SYSROOT}${PREFIX}/include"
 
 # Mostly taken from myunix3 mk/toolchain.mk or gcc --verbose on debian
 # --enable-multilib: build different libgcc to support -march (i think)
@@ -98,19 +93,17 @@ mkdir -p "${SYSROOT}/include"
     --disable-multilib \
     --with-arch=rv64imafdc \
     --with-abi=lp64 \
-    --prefix="${SYSROOT}" \
+    --prefix="${SYSROOT}${PREFIX}" \
     --disable-nls \
     --with-system-zlib \
-    --disable-threads \
-    --with-sysroot="${SYSROOT}" \
-    --with-native-system-header-dir="/include"
+    --disable-threads
 
 popd
 
 # build riscv64-linux-gnu-gcc cross compiler
 # Seperate make invocations because gccs Makefile is fragile
-make -C gcc-build -j3 --output-sync all-gcc
-make -C gcc-build --output-sync install-gcc
+make -C gcc-build ${JOBS} all-gcc
+make -C gcc-build ${JOBS} install-gcc
 
 # Build musl-libc
 
@@ -119,35 +112,32 @@ CFLAGS="-march=rv64iac" \
 ./configure \
     --target=riscv64-linux-gnu \
     --disable-shared \
-    --prefix="${SYSROOT}"
+    --prefix="${SYSROOT}${PREFIX}"
 popd
 
-make -C musl-1.2.2 -j3 --output-sync all
-make -C musl-1.2.2 -j3 --output-sync install
+make -C musl-1.2.2 ${JOBS} all
+make -C musl-1.2.2 ${JOBS} install
 
 # Build gcc step 2
 
 # build libgcc using installed headers
-make -C gcc-build -j3 --output-sync all-target-libgcc
-make -C gcc-build -j3 --output-sync install-target-libgcc
+make -C gcc-build ${JOBS} all-target-libgcc
+make -C gcc-build ${JOBS} install-target-libgcc
 
 ## Cleanup
 
 # Remove zips
-rm binutils-2.35.2.tar.xz
-rm binutils-2.35.2.tar.xz.sig
-rm gcc-10.2.0.tar.xz
-rm gcc-10.2.0.tar.xz.sig
-rm musl-1.2.2.tar.gz
-rm musl-1.2.2.tar.gz.asc
+rm -f binutils-2.35.2.tar.xz \
+    gcc-10.2.0.tar.xz \
+    musl-1.2.2.tar.gz
 
 # Remove source dirs
-rm -rf binutils-2.35.2
-rm -rf gcc-10.2.0
-rm -rf musl-1.2.2
+rm -rf binutils-2.35.2 \
+    gcc-10.2.0 \
+    musl-1.2.2
 
 # Remove build dirs
-rm -rf binutils-build
-rm -rf gcc-build
+rm -rf binutils-build \
+    gcc-build
 
 popd
