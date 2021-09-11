eac9ce3f2410348cb1ca78cb5d0e78cdfadd3fb7
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-09-11 15:05:29 +0200

    lifter: enable tests for floats (still fails with SIGSEGV)

--- src/lifter/tests/test_float.cpp
+++ src/lifter/tests/test_float.cpp
@@ -7,8 +7,6 @@ using namespace lifter::RV64;
 
 using reg_map = Lifter::reg_map;
 
-#if 0
-
 class TestFloatingPointLifting : public ::testing::Test {
   public:
     IR *ir;
@@ -1305,5 +1303,3 @@ TEST_F(TestFloatingPointLifting, test_fp_fled) {
     const RV64Inst instr{FrvInst{FRV_FLED, 27, 3, 15, 0, 0, 0}, 4};
     test_fp_compare_lifting(instr);
 }
-
-#endif
