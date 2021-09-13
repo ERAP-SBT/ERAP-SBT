1f937ce9fa49019554499d26da349e3127e6ea7f
Felix Solcher <felix.solcher@tum.de>
2021-09-13 15:01:21 +0200

    Add tests for simplify_double_op

--- src/ir/tests/test_const_folding.cpp
+++ src/ir/tests/test_const_folding.cpp
@@ -86,21 +86,90 @@ TEST(TestConstFolding, test_simplify) {
     ASSERT_EQ(o.target_inputs()[0]->id, a->id);
 }
 
-TEST(TestConstFolding, test_double) {
+enum class Side {
+    Any,
+    Left,
+    Right,
+};
+
+struct DoubleTestCase {
+    Instruction first_insn;
+    uint64_t first_imm;
+    Side first_imm_side;
+
+    Instruction second_insn;
+    uint64_t second_imm;
+    Side second_imm_side;
+
+    Instruction result_insn;
+    uint64_t result_imm;
+    Side result_imm_side;
+};
+
+// operator<< implementations for assertion pretty printing
+
+std::ostream &operator<<(std::ostream &out, Side side) {
+    auto side_name = [](Side side) -> const char * {
+        switch (side) {
+        case Side::Any:
+            return "any";
+        case Side::Left:
+            return "left";
+        case Side::Right:
+            return "right";
+        }
+        __builtin_unreachable();
+    };
+    out << side_name(side);
+    return out;
+}
+
+std::ostream &operator<<(std::ostream &out, const DoubleTestCase &tc) {
+    out << "{ first: {" << tc.first_insn << ", " << tc.first_imm << ", " << tc.first_imm_side << "}, "
+        << "second: {" << tc.second_insn << ", " << tc.second_imm << ", " << tc.second_imm_side << "}, "
+        << "result: {" << tc.result_insn << ", " << tc.result_imm << ", " << tc.result_imm_side << "} }";
+    return out;
+}
+
+class DoubleFoldTest : public testing::TestWithParam<DoubleTestCase> {};
+
+TEST_P(DoubleFoldTest, DoesFoldIntoOneVar) {
+    DoubleTestCase test_case = GetParam();
+
+    assert(test_case.first_imm_side != Side::Any && test_case.second_imm_side != Side::Any);
+
     IR ir;
     ir.add_static(Type::i32);
     auto *bb = ir.add_basic_block();
 
-    auto *a = bb->add_var_from_static(0);
-    auto *b = bb->add_var_imm(1, 0);
+    auto *first_stc = bb->add_var_from_static(0);
+    auto *first_imm = bb->add_var_imm(test_case.first_imm, 0);
 
-    auto *c = bb->add_var(Type::i32, 1);
-    c->set_op(Operation::new_add(c, a, b));
+    auto *first = bb->add_var(Type::i32, 1);
+    {
+        auto op = std::make_unique<Operation>(test_case.first_insn);
+        op->set_outputs(first);
+        if (test_case.first_imm_side == Side::Left) {
+            op->set_inputs(first_imm, first_stc);
+        } else {
+            op->set_inputs(first_stc, first_imm);
+        }
+        first->set_op(std::move(op));
+    }
 
-    auto *d = bb->add_var_imm(2, 2);
+    auto *second_imm = bb->add_var_imm(test_case.second_imm, 2);
 
-    auto *e = bb->add_var(Type::i32, 3);
-    e->set_op(Operation::new_add(e, c, d));
+    auto *second = bb->add_var(Type::i32, 3);
+    {
+        auto op = std::make_unique<Operation>(test_case.second_insn);
+        op->set_outputs(second);
+        if (test_case.second_imm_side == Side::Left) {
+            op->set_inputs(second_imm, first);
+        } else {
+            op->set_inputs(first, second_imm);
+        }
+        second->set_op(std::move(op));
+    }
 
     assert_valid(ir);
 
@@ -108,12 +177,61 @@ TEST(TestConstFolding, test_double) {
 
     assert_valid(ir);
 
-    ASSERT_TRUE(e->is_operation());
-    const auto &op = e->get_operation();
+    {
+        auto *result = second;
+        ASSERT_TRUE(result->is_operation());
 
-    ASSERT_TRUE(op.in_vars[1] && op.in_vars[1]->is_static());
-    ASSERT_EQ(op.in_vars[1]->get_static(), 0);
+        const auto &op = result->get_operation();
+        ASSERT_TRUE(op.in_vars[0] && op.in_vars[1]);
 
-    ASSERT_TRUE(op.in_vars[0] && op.in_vars[0]->is_immediate());
-    ASSERT_EQ(op.in_vars[0]->get_immediate().val, 3);
+        SSAVar *result_imm, *result_stc;
+        Side result_imm_side;
+        if (op.in_vars[0]->is_immediate()) {
+            result_imm = op.in_vars[0].get();
+            result_stc = op.in_vars[1].get();
+            result_imm_side = Side::Left;
+        } else {
+            result_imm = op.in_vars[1].get();
+            result_stc = op.in_vars[0].get();
+            result_imm_side = Side::Right;
+        }
+
+        ASSERT_TRUE(result_imm->is_immediate());
+        ASSERT_EQ(result_imm->get_immediate().val, test_case.result_imm);
+        ASSERT_TRUE(result_stc->is_static());
+        ASSERT_EQ(result_stc->get_static(), first_stc->get_static());
+
+        ASSERT_EQ(op.type, test_case.result_insn);
+
+        if (test_case.result_imm_side != Side::Any) {
+            ASSERT_EQ(result_imm_side, test_case.result_imm_side);
+        }
+    }
 }
+
+INSTANTIATE_TEST_SUITE_P(DoubleInst, DoubleFoldTest,
+                         testing::ValuesIn({
+                             DoubleTestCase{Instruction::add, 1, Side::Right, Instruction::add, 2, Side::Right, Instruction::add, 3, Side::Any},
+                             DoubleTestCase{Instruction::add, 1, Side::Left, Instruction::add, 2, Side::Right, Instruction::add, 3, Side::Any},
+                             DoubleTestCase{Instruction::add, 1, Side::Right, Instruction::add, 2, Side::Left, Instruction::add, 3, Side::Any},
+                             DoubleTestCase{Instruction::add, 1, Side::Left, Instruction::add, 2, Side::Left, Instruction::add, 3, Side::Any},
+
+                             DoubleTestCase{Instruction::add, 6, Side::Right, Instruction::sub, 4, Side::Right, Instruction::add, 2, Side::Any},
+                             DoubleTestCase{Instruction::add, 6, Side::Left, Instruction::sub, 4, Side::Right, Instruction::add, 2, Side::Any},
+                             DoubleTestCase{Instruction::add, 4, Side::Right, Instruction::sub, 7, Side::Left, Instruction::sub, 3, Side::Left},
+                             DoubleTestCase{Instruction::add, 4, Side::Left, Instruction::sub, 7, Side::Left, Instruction::sub, 3, Side::Left},
+
+                             DoubleTestCase{Instruction::sub, 4, Side::Right, Instruction::add, 7, Side::Right, Instruction::add, 3, Side::Any},
+                             DoubleTestCase{Instruction::sub, 4, Side::Right, Instruction::add, 7, Side::Left, Instruction::add, 3, Side::Any},
+                             DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::add, 4, Side::Right, Instruction::sub, 10, Side::Left},
+                             DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::add, 4, Side::Left, Instruction::sub, 10, Side::Left},
+
+                             DoubleTestCase{Instruction::sub, 4, Side::Right, Instruction::sub, 5, Side::Right, Instruction::sub, 9, Side::Right},
+                             DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::sub, 4, Side::Right, Instruction::sub, 2, Side::Left},
+                             DoubleTestCase{Instruction::sub, 3, Side::Right, Instruction::sub, 2, Side::Left, Instruction::sub, 5, Side::Left},
+                             DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::sub, 9, Side::Left, Instruction::add, 3, Side::Left},
+
+                             DoubleTestCase{Instruction::_and, 0b110, Side::Right, Instruction::_and, 0b011, Side::Right, Instruction::_and, 0b010, Side::Any},
+                             DoubleTestCase{Instruction::_or, 0b110, Side::Right, Instruction::_or, 0b011, Side::Right, Instruction::_or, 0b111, Side::Any},
+                             DoubleTestCase{Instruction::_xor, 0b110, Side::Right, Instruction::_xor, 0b011, Side::Right, Instruction::_xor, 0b101, Side::Any},
+                         }));
