4b51b0e857b0f22914030f56fa48a8e199f8e95c
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-09-15 22:07:15 +0200

    helloworld3: modify example to trigger interpreter

--- examples/helloworld3.c
+++ examples/helloworld3.c
@@ -11,11 +11,25 @@ static uint64_t fib(uint32_t n) {
     }
 }
 
+#if 1
+/* This NOP causes the lifter to not recognize a basic block correctly, forcing the interpreter to run
+ */
+__attribute__((naked)) void f1(void) {
+    __asm__ volatile("nop" : : : "memory");
+}
+#endif
+
 static void fib_test(uint32_t n) {
     printf("Calculating fib(%u): ", n);
     fflush(stdout);
     printf("%lu\n", fib(n));
 }
+
+typedef void (*func_type)(uint32_t n);
+
+// volatile: force compiler to write value into memory and read it back, so this won't be recognized as
+// basicblock
+volatile func_type a;
 
 int main(int argc, char *argv[]) {
     printf("Hello World from C + musl libc\n");
@@ -25,7 +39,11 @@ int main(int argc, char *argv[]) {
         printf("argv[%u]: %s\n", i, argv[i]);
     }
 
-    fib_test(35);
+    a = &fib_test;
+
+    __asm__ volatile("nop" : : : "memory");
+
+    a(2);
 
     return EXIT_SUCCESS;
 }
