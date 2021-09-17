ac3e12902a5a2c8419df0698f6b9b64caa88fc8e
Felix Solcher <felix.solcher@tum.de>
2021-09-17 14:57:12 +0200

    Dead block input elimination

--- include/ir/operation.h
+++ include/ir/operation.h
@@ -144,6 +144,7 @@ struct CfOp {
     void set_target(BasicBlock *target);
 
     BasicBlock *target() const;
+    BasicBlock *continuation_target() const;
 
     /**
      * @brief Returns a list of target inputs, depending on the type.
