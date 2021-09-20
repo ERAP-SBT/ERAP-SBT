07b14556975c343a44c16fba6d6102d3b6584306
Felix Solcher <felix.solcher@tum.de>
2021-09-20 23:01:14 +0200

    Add tests for binary-relative immediates

--- src/ir/tests/test_const_folding.cpp
+++ src/ir/tests/test_const_folding.cpp
@@ -83,6 +83,12 @@ enum class Side {
     Right,
 };
 
+enum class BinaryRelative {
+    None,
+    First,
+    Second,
+};
+
 struct DoubleTestCase {
     Instruction first_insn;
     uint64_t first_imm;
@@ -95,30 +101,69 @@ struct DoubleTestCase {
     Instruction result_insn;
     uint64_t result_imm;
     Side result_imm_side;
+
+    BinaryRelative bin_rel{BinaryRelative::None};
 };
 
-// operator<< implementations for assertion pretty printing
-
-std::ostream &operator<<(std::ostream &out, Side side) {
-    auto side_name = [](Side side) -> const char * {
-        switch (side) {
-        case Side::Any:
-            return "any";
-        case Side::Left:
-            return "left";
-        case Side::Right:
-            return "right";
-        }
-        __builtin_unreachable();
-    };
-    out << side_name(side);
-    return out;
-}
+// operator<< implementation for assertion pretty printing
 
 std::ostream &operator<<(std::ostream &out, const DoubleTestCase &tc) {
-    out << "{ first: {" << tc.first_insn << ", " << tc.first_imm << ", " << tc.first_imm_side << "}, "
-        << "second: {" << tc.second_insn << ", " << tc.second_imm << ", " << tc.second_imm_side << "}, "
-        << "result: {" << tc.result_insn << ", " << tc.result_imm << ", " << tc.result_imm_side << "} }";
+    auto print_imm = [&](uint64_t imm, bool bin_rel) {
+        out << imm;
+        if (bin_rel) {
+            out << "br";
+        }
+    };
+    auto print_inner_op = [&]() {
+        out << '(' << tc.first_insn << ' ';
+        switch (tc.first_imm_side) {
+        case Side::Left:
+            print_imm(tc.first_imm, tc.bin_rel == BinaryRelative::First);
+            out << ", x";
+            break;
+        case Side::Right:
+            out << "x, ";
+            print_imm(tc.first_imm, tc.bin_rel == BinaryRelative::First);
+            break;
+        case Side::Any:
+            assert(false && "Invalid test configuration");
+            break;
+        }
+        out << ')';
+    };
+
+    out << '(' << tc.second_insn << ' ';
+    switch (tc.second_imm_side) {
+    case Side::Left:
+        print_imm(tc.second_imm, tc.bin_rel == BinaryRelative::Second);
+        out << ", ";
+        print_inner_op();
+        break;
+    case Side::Right:
+        print_inner_op();
+        out << ", ";
+        print_imm(tc.second_imm, tc.bin_rel == BinaryRelative::Second);
+        break;
+    case Side::Any:
+        assert(false && "Invalid test configuration");
+        break;
+    }
+    out << ") -> (" << tc.result_insn << ' ';
+    switch (tc.result_imm_side) {
+    case Side::Left:
+        print_imm(tc.result_imm, tc.bin_rel != BinaryRelative::None);
+        out << ", x";
+        break;
+    case Side::Right:
+    case Side::Any:
+        out << "x, ";
+        print_imm(tc.result_imm, tc.bin_rel != BinaryRelative::None);
+        break;
+    }
+    if (tc.result_imm_side == Side::Any) {
+        out << " [commutative]";
+    }
+    out << ')';
     return out;
 }
 
@@ -134,7 +179,7 @@ TEST_P(DoubleFoldTest, DoesFoldIntoOneVar) {
     auto *bb = ir.add_basic_block();
 
     auto *first_stc = bb->add_var_from_static(0);
-    auto *first_imm = bb->add_var_imm(test_case.first_imm, 0);
+    auto *first_imm = bb->add_var_imm(test_case.first_imm, 0, test_case.bin_rel == BinaryRelative::First);
 
     auto *first = bb->add_var(Type::i32, 1);
     {
@@ -148,7 +193,7 @@ TEST_P(DoubleFoldTest, DoesFoldIntoOneVar) {
         first->set_op(std::move(op));
     }
 
-    auto *second_imm = bb->add_var_imm(test_case.second_imm, 2);
+    auto *second_imm = bb->add_var_imm(test_case.second_imm, 2, test_case.bin_rel == BinaryRelative::Second);
 
     auto *second = bb->add_var(Type::i32, 3);
     {
@@ -197,6 +242,9 @@ TEST_P(DoubleFoldTest, DoesFoldIntoOneVar) {
         if (test_case.result_imm_side != Side::Any) {
             ASSERT_EQ(result_imm_side, test_case.result_imm_side);
         }
+
+        // If one of the inputs is binary-relative, the result (at least now) has to be binary-relative, too.
+        ASSERT_EQ(result_imm->get_immediate().binary_relative, test_case.bin_rel != BinaryRelative::None);
     }
 }
 
@@ -207,20 +255,34 @@ INSTANTIATE_TEST_SUITE_P(DoubleInst, DoubleFoldTest,
                              DoubleTestCase{Instruction::add, 1, Side::Right, Instruction::add, 2, Side::Left, Instruction::add, 3, Side::Any},
                              DoubleTestCase{Instruction::add, 1, Side::Left, Instruction::add, 2, Side::Left, Instruction::add, 3, Side::Any},
 
+                             DoubleTestCase{Instruction::add, 1, Side::Right, Instruction::add, 2, Side::Right, Instruction::add, 3, Side::Any, BinaryRelative::First},
+                             DoubleTestCase{Instruction::add, 1, Side::Left, Instruction::add, 2, Side::Right, Instruction::add, 3, Side::Any, BinaryRelative::Second},
+                             DoubleTestCase{Instruction::add, 1, Side::Right, Instruction::add, 2, Side::Left, Instruction::add, 3, Side::Any, BinaryRelative::Second},
+                             DoubleTestCase{Instruction::add, 1, Side::Left, Instruction::add, 2, Side::Left, Instruction::add, 3, Side::Any, BinaryRelative::First},
+
                              DoubleTestCase{Instruction::add, 6, Side::Right, Instruction::sub, 4, Side::Right, Instruction::add, 2, Side::Any},
                              DoubleTestCase{Instruction::add, 6, Side::Left, Instruction::sub, 4, Side::Right, Instruction::add, 2, Side::Any},
                              DoubleTestCase{Instruction::add, 4, Side::Right, Instruction::sub, 7, Side::Left, Instruction::sub, 3, Side::Left},
                              DoubleTestCase{Instruction::add, 4, Side::Left, Instruction::sub, 7, Side::Left, Instruction::sub, 3, Side::Left},
+
+                             DoubleTestCase{Instruction::add, 6, Side::Right, Instruction::sub, 4, Side::Right, Instruction::add, 2, Side::Any, BinaryRelative::First},
+                             DoubleTestCase{Instruction::add, 6, Side::Left, Instruction::sub, 4, Side::Right, Instruction::add, 2, Side::Any, BinaryRelative::First},
 
                              DoubleTestCase{Instruction::sub, 4, Side::Right, Instruction::add, 7, Side::Right, Instruction::add, 3, Side::Any},
                              DoubleTestCase{Instruction::sub, 4, Side::Right, Instruction::add, 7, Side::Left, Instruction::add, 3, Side::Any},
                              DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::add, 4, Side::Right, Instruction::sub, 10, Side::Left},
                              DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::add, 4, Side::Left, Instruction::sub, 10, Side::Left},
 
+                             DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::add, 4, Side::Right, Instruction::sub, 10, Side::Left, BinaryRelative::First},
+                             DoubleTestCase{Instruction::add, 6, Side::Left, Instruction::sub, 4, Side::Right, Instruction::add, 2, Side::Any, BinaryRelative::First},
+
                              DoubleTestCase{Instruction::sub, 4, Side::Right, Instruction::sub, 5, Side::Right, Instruction::sub, 9, Side::Right},
                              DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::sub, 4, Side::Right, Instruction::sub, 2, Side::Left},
                              DoubleTestCase{Instruction::sub, 3, Side::Right, Instruction::sub, 2, Side::Left, Instruction::sub, 5, Side::Left},
                              DoubleTestCase{Instruction::sub, 6, Side::Left, Instruction::sub, 9, Side::Left, Instruction::add, 3, Side::Left},
+
+                             DoubleTestCase{Instruction::sub, 4, Side::Right, Instruction::sub, 6, Side::Right, Instruction::sub, 10, Side::Right, BinaryRelative::Second},
+                             DoubleTestCase{Instruction::sub, 4, Side::Left, Instruction::sub, 6, Side::Left, Instruction::add, 2, Side::Any, BinaryRelative::Second},
 
                              DoubleTestCase{Instruction::_and, 0b110, Side::Right, Instruction::_and, 0b011, Side::Right, Instruction::_and, 0b010, Side::Any},
                              DoubleTestCase{Instruction::_or, 0b110, Side::Right, Instruction::_or, 0b011, Side::Right, Instruction::_or, 0b111, Side::Any},
