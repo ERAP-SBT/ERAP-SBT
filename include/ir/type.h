8b28c592e416168a5277046687bd9277b9877c87
Johannes Maier <johannes2.maier@tum.de>
2021-09-22 18:05:21 +0200

    Changed implementation of cast_dir

--- include/ir/type.h
+++ include/ir/type.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <ostream>
 
 /* [i64, i32, i16, i8]: Integer types, signness is defined by the operation
@@ -9,44 +10,20 @@
  */
 enum class Type { imm, i64, i32, i16, i8, f64, f32, mt };
 
-constexpr std::array<Type, 5> i_cast_order = {Type::i8, Type::i16, Type::i32, Type::i64, Type::imm};
-constexpr std::array<Type, 2> f_cast_order = {Type::f32, Type::f64};
+constexpr std::array<std::array<int, 8>, 8> cast_table = {std::array<int, 8>{0, 0, 0, 0, 0, -1, -1, -1},
+                                                          {1, 0, 0, 0, 0, -1, -1, -1},
+                                                          {1, 1, 0, 0, 0, -1, -1, -1},
+                                                          {1, 1, 1, 0, 0, -1, -1, -1},
+                                                          {1, 1, 1, 1, 0, -1, -1, -1},
+                                                          {-1, -1, -1, -1, -1, 0, 0, -1},
+                                                          {-1, -1, -1, -1, -1, 1, 0, -1},
+                                                          {-1, -1, -1, -1, -1, -1, -1, -1}};
 
 /* -1: no simple cast possible (e.g. integer <-> float)
  * 0: t1 !< t2
  * 1: t1 < t2
  */
-constexpr int cast_dir(const Type t1, const Type t2) {
-    size_t t1_i_idx = i_cast_order.size();
-    size_t t2_i_idx = i_cast_order.size();
-    for (size_t i = 0; i < i_cast_order.size(); i++) {
-        if (i_cast_order[i] == t1) {
-            t1_i_idx = i;
-        }
-        if (i_cast_order[i] == t2) {
-            t2_i_idx = i;
-        }
-    }
-    if (t1_i_idx < i_cast_order.size() && t2_i_idx < i_cast_order.size()) {
-        return t1_i_idx < t2_i_idx;
-    }
-
-    size_t t1_f_idx = f_cast_order.size();
-    size_t t2_f_idx = f_cast_order.size();
-    for (size_t i = 0; i < f_cast_order.size(); i++) {
-        if (f_cast_order[i] == t1) {
-            t1_f_idx = i;
-        }
-        if (f_cast_order[i] == t2) {
-            t2_f_idx = i;
-        }
-    }
-    if (t1_f_idx < f_cast_order.size() && t2_f_idx < f_cast_order.size()) {
-        return t1_f_idx < t2_f_idx;
-    }
-
-    return -1;
-}
+constexpr int cast_dir(const Type t1, const Type t2) { return cast_table[static_cast<size_t>(t1)][static_cast<size_t>(t2)]; }
 
 constexpr bool is_float(const Type type) { return type == Type::f32 || type == Type::f64; }
 
