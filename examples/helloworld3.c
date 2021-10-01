ed77e2de86dc7f2545f91a60f4f909b323ec8766
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-10-01 22:01:17 +0200

    helloworld3: extend float test

--- examples/helloworld3.c
+++ examples/helloworld3.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
 
 static uint64_t fib(uint32_t n) {
     if (n <= 1) {
@@ -31,19 +32,50 @@ typedef void (*func_type)(uint32_t n);
 // basicblock
 volatile func_type a;
 
+void test3_testf(float v) {
+    assert(v == 10.1f);
+}
+
+void test3_good(void) {
+    printf("test3_good: 10.1 == %f\n", 10.1f);
+}
+
+void test3_bad(void) {
+    volatile float v = 10.1f;
+    printf("test3_bad: 10.1 == %f\n", v);
+}
+
+void test3(void) {
+    printf("test3: 10.1 == %f\n", 10.1f);
+    test3_testf(10.1f);
+
+    volatile float v = 10.1f;
+    printf("test3: 10.1 == %f\n", v);
+    test3_testf(v);
+
+    test3_good();
+    test3_bad();
+}
+
 int main(int argc, char *argv[]) {
     printf("Hello World from C + libc\n");
 
     assert(strstr("1234", "34") != NULL);
 
     {
-        float v1;
+        volatile float v1 = 1.23f;
         int64_t v2;
         assert(sscanf("10.1 12345", "%f %d", &v1, &v2) == 2);
         printf("v1 = %f\n", v1); /* TODO: currently delivers wrong values when build with --interpreter-only=true */
         printf("v2 = %d\n", v2);
         assert(v1 == 10.100000f);
         assert(v2 == 12345);
+
+        printf("nan = %f\n", NAN);
+        printf("inf = %f\n", INFINITY);
+        printf("1.0 = %f\n", 1.0f);
+        printf("2.0 = %f\n", 2.0f);
+        printf("10.1 = %f\n", 10.1f);
     }
 
     {
@@ -54,7 +86,11 @@ int main(int argc, char *argv[]) {
         assert(v1 == v2);
         assert(v1 != v3);
         assert(v2 != v3);
+
+        printf("10.1 = %f\n", v1);
     }
+
+    test3();
 
     assert(strcmp("abc", "abc2") < 0);
 
