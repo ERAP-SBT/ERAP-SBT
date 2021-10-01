46b7c09004cea77310a30d98319afced355c27ea
Johannes Maier <johannes2.maier@tum.de>
2021-10-01 12:07:30 +0200

    Remove useless comments

--- src/generator/x86_64/generator.cpp
+++ src/generator/x86_64/generator.cpp
@@ -549,8 +549,6 @@ void Generator::compile_vars(const BasicBlock *block) {
                 assert(!is_float(var->type));
                 assert(compatible_types(var->type, in3->type) && compatible_types(var->type, in4->type));
                 fprintf(out_fd, "comis%s %s, %s\n", fp_op_size_from_type(in1->type), in_regs[0], in_regs[1]);
-                // fprintf(out_fd, "cmov%s %s, %s\n", cc_fp_1, rax_from_type(var->type), op_reg_map_for_type(var->type)[2]);
-                // fprintf(out_fd, "cmov%s %s, %s\n", cc_fp_2, rax_from_type(var->type), op_reg_map_for_type(var->type)[3]);
                 cc_1 = cc_fp_1;
                 cc_2 = cc_fp_2;
             } else {
@@ -700,11 +698,6 @@ void Generator::compile_vars(const BasicBlock *block) {
             set_if_op("l", "ge", "b", "ae");
             break;
         case Instruction::sltu:
-            // assert(arg_count == 4);
-            // assert(!is_float(op->in_vars[0]->type));
-            // fprintf(out_fd, "cmp %s, %s\n", in_regs[0], in_regs[1]);
-            // fprintf(out_fd, "cmovb %s, %s\n", in_regs[0], in_regs[2]);
-            // fprintf(out_fd, "cmovae %s, %s\n", in_regs[0], in_regs[3]);
             set_if_op("b", "ae", "", "", false);
             break;
         case Instruction::sumul_h: /* TODO: implement */
@@ -745,29 +738,9 @@ void Generator::compile_vars(const BasicBlock *block) {
             }
             break;
         case Instruction::sle:
-            // assert(arg_count == 4);
-            // assert(is_float(op->in_vars[0]->type));
-            // assert(op->in_vars[0]->type == op->in_vars[1]->type);
-            // fprintf(out_fd, "comis%s %s, %s\n", fp_op_size_from_type(op->in_vars[0]->type), in_regs[0], in_regs[1]);
-            // fprintf(out_fd, "cmovbe %s, %s\n", rax_from_type(var->type), in_regs[2]);
-            // fprintf(out_fd, "cmova %s, %s\n", rax_from_type(var->type), in_regs[3]);
             set_if_op("le", "g", "be", "a");
             break;
         case Instruction::seq:
-            /*assert(arg_count == 4);
-            assert(op->in_vars[0]->type == op->in_vars[1]->type || (!is_float(op->in_vars[0]->type) && op->in_vars[1]->type == Type::imm) ||
-                   (op->in_vars[0]->type == Type::imm && !is_float(op->in_vars[1]->type)));
-            assert(op->in_vars[2]->type == op->in_vars[3]->type);
-            assert(!is_float(op->in_vars[2]->type));
-
-            if (is_float(op->in_vars[0]->type)) {
-                fprintf(out_fd, "comis%s %s, %s\n", fp_op_size_from_type(op->in_vars[0]->type), in_regs[0], in_regs[1]);
-            } else {
-                fprintf(out_fd, "cmp %s, %s\n", in_regs[0], in_regs[1]);
-            }
-
-            fprintf(out_fd, "cmove %s, %s\n", rax_from_type(var->type), in_regs[2]);
-            fprintf(out_fd, "cmovne %s, %s\n", rax_from_type(var->type), in_regs[3]); */
             set_if_op("e", "ne", "e", "ne");
             break;
         case Instruction::fmul:
