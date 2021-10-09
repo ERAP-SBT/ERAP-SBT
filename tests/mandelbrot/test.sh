d70a3546d17ab116cac46b748f148ebffcbf13a1
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-10-10 00:12:03 +0200

    mandelbrot: compare correct things with each other in the correct way

--- tests/mandelbrot/test.sh
+++ tests/mandelbrot/test.sh
@@ -27,8 +27,9 @@ mkdir build_rv64
 
 set -x
 
+gcc -g -static -o build_amd64/mandelbrot mandelbrot.c  -Wall -Wextra -O3
 # -march=native: generate fma3 instructions if supported
-gcc -march=native -g -static -o build_amd64/mandelbrot mandelbrot.c  -Wall -Wextra -O3
+gcc -march=native -g -static -o build_amd64/mandelbrot_fma mandelbrot.c  -Wall -Wextra -O3
 $1 -g -static -o build_rv64/mandelbrot mandelbrot.c -Wall -Wextra -O3
 
 { set +x; } 2>/dev/null
@@ -49,11 +50,12 @@ echo -e "${TXT_BLUE}Testing for the right result...${TXT_CLEAR}"
 set -x
 
 build_amd64/mandelbrot > amd64_mandelbrot.txt
+build_amd64/mandelbrot_fma > amd64_mandelbrot_fma.txt
 build_rv64/translated > rv64_mandelbrot.txt
 build_rv64/interpreter > interpreter_mandelbrot.txt
 
 cmp amd64_mandelbrot.txt rv64_mandelbrot.txt
-cmp amd64_mandelbrot.txt interpreter_mandelbrot.txt
+cmp amd64_mandelbrot_fma.txt interpreter_mandelbrot.txt
 
 { set +x; } 2>/dev/null
 
@@ -63,7 +65,7 @@ echo -e "${TXT_BLUE}Cleaning up...${TXT_CLEAR}"
 set -x
 
 rm -rf build_amd64 build_rv64
-rm amd64_mandelbrot.txt rv64_mandelbrot.txt interpreter_mandelbrot.txt
+rm amd64_mandelbrot.txt amd64_mandelbrot_fma.txt rv64_mandelbrot.txt interpreter_mandelbrot.txt
 
 { set +x; } 2>/dev/null
 exit 0
