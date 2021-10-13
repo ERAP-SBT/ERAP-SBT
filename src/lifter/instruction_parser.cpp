2ea1b0da53c4719daefeced64ec60e5d689eb21d
Jonathan Hettwer <jonathan.hettwer@tum.de>
2021-10-13 02:32:07 +0200

    test: success

--- src/lifter/instruction_parser.cpp
+++ src/lifter/instruction_parser.cpp
@@ -403,6 +403,11 @@ void Lifter::parse_instruction(BasicBlock *bb, const RV64Inst &instr, reg_map &m
 
         /* D Extension */
 
+#if 1
+    default:
+        lift_jump_interpreter(bb, instr, mapping, ip);
+        break;
+#else
     case FRV_FLD:
         lift_load(bb, instr, mapping, ip, Type::f64, false);
         break;
@@ -450,11 +455,6 @@ void Lifter::parse_instruction(BasicBlock *bb, const RV64Inst &instr, reg_map &m
     case FRV_FSGNJXD:
         lift_float_sign_injection(bb, instr, mapping, ip, Type::f64);
         break;
-#if 1
-    default:
-        lift_jump_interpreter(bb, instr, mapping, ip);
-        break;
-#else
     case FRV_FMIND:
         lift_float_two_operands(bb, instr, mapping, ip, Instruction::min, Type::f64);
         break;
